08984034f6e2ec857a4277e0ad5c43203071ba22
Tsquare25 <tibebe1234t@gmail.com>
2021-11-03 14:43:36 +0000

    hero section mockup done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,35 +1,90 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { Color } from "../../data/Color";
+import { HeroData } from "../../data/HeroData";
+import { Button } from "../Button";
 const Hero = ({ SliderData }) => {
   console.log(SliderData);
   return (
     <HeroContainer>
       <HeroWrapper>
-        <HeroContent></HeroContent>
+        <HeroContent>
+          <Text>
+            <h1>{HeroData.title}</h1>
+            <span>{HeroData.paragraph}</span>
+            <Button reverse={false} to={HeroData.buttonPath}>
+              {HeroData.buttonLabel}
+            </Button>
+          </Text>
+          <Image>
+            <img src={HeroData.image} alt="injera" />
+          </Image>
+        </HeroContent>
       </HeroWrapper>
     </HeroContainer>
   );
 };
 const Text = styled.div`
-  font-weight: 700;
+  justify-self: flex-start;
+  z-index: 2;
   color: ${Color.white};
+  display: grid;
+  justify-content: center;
+  place-items: start;
 
+  h1 {
+    font-size: clamp(2rem, 8vw, 5rem);
+    padding: 0px;
+    margin: 0px;
+    border: 0;
+    font-weight: 700;
+  }
   span {
+    font-size: clamp(1rem, 4vw, 2rem);
+    margin: 0 0 1rem 0;
     color: ${Color.red};
+  }
+  .Button {
+    background: red;
   }
 `;
 
-const HeroContent = styled.div`
-  width: 1000px;
-  max-width: 90vw;
-  height: 600px;
-  background: ${Color.orange};
+const Image = styled.div`
+  position: relative;
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
+    border-radius: 10px;
+    border: 1px solid ${Color.black};
   }
+
+  &::before {
+    content: "";
+    display: block;
+    z-index: 999;
+    background: linear-gradient(0deg, ${Color.black} 0%, ${Color.white} 100%);
+    opacity: 0.2;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+  }
+`;
+
+const HeroContent = styled.div`
+  width: 1500px;
+  max-width: 90vw;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4rem;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    gap: 2rem;
+  } ;
 `;
 const HeroContainer = styled.div`
   width: 100%;
---
+++ src/data/HeroData.js
@@ -0,0 +1,10 @@
+import ImageOne from "./../images/Injera-1.jpg";
+
+export const HeroData = {
+  title: "True Favour",
+  paragraph: "Exprience authentic Ethiopian cuisine",
+  button: true,
+  buttonLabel: "View Our Menu",
+  buttonPath: "/menu",
+  image: ImageOne,
+};
