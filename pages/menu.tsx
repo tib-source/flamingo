a18dc77cbe7aae4044df9d0eabdcf1b01c4d80d4
Tsquare25 <tibebe1234t@gmail.com>
2021-12-04 22:44:41 +0000

    Finally fixed the menu.md file - indentation fixed

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -11,9 +11,9 @@ import { flex } from "../components/Util/flex";
 import { Color } from "../data/Color";
 import Kitfo from "../public/images/kitfo.jpg";
 import Meta from "../components/Meta";
-// import { attributes } from "./../content/menu.md";
+import { attributes } from "./../content/menu.md";
 import { MenuItem } from "../components/types";
-import { MenuData } from "../data/Menu";
+// import { MenuData } from "../data/Menu";
 
 const Container = styled(flex)`
   ${containerCss}
@@ -137,7 +137,7 @@ const Card = styled.div`
 
 const Menu = () => {
   type MenuList = MenuItem[]
-  // const { title, MenuData }: { title: string, MenuData: MenuList } = attributes
+  const { title, MenuData }: { title: string, MenuData: MenuList } = attributes
 
   const Orignial = MenuData;
   const [Menu, setMenu] = useState<MenuList>(MenuData)
